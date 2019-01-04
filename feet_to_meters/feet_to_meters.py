6ed6987fc0397ea0b6a5861f06dce8fa6472f54e
AlisonWonderland <dimitridawn@gmail.com>
2019-01-03 20:46:44 -0800

    Checking to see if I can upload folder

---
+++ feet_to_meters/feet_to_meters.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import praw
+import re
+
+def convertToMeter(feet):
+    meters = feet * 3.28084
+
+def extractNumber(feetString):
+    feet = ''
+    i = 0
+    
+    while(str.isdigit(feetString[i])):
+        feet += feetString[i]
+        i += 1
+
+    convertToMeter(int(feet))
+
+def findFeet(sentence):
+    ftRegex = re.compile('[0-9]+ ft')
+    feetRegex = re.compile('[0-9]+ feet')
+    
+    ftMatch = ftRegex.search(sentence)
+    feetMatch = feetRegex.search(sentence)
+    
+    if (ftMatch == None and feetMatch == None):
+        return
+    
+    elif (ftMatch != None):
+        extractNumber(ftMatch.group())
+    
+    else: #means feetMatch !=None
+        extractNumber(feetMatch.group())
+
+def main():
+    reddit = praw.Reddit('bot1')
+    subreddit = reddit.subreddit('diariesfrom_me')
+    
+    for submission in subreddit.hot(limit = 25):
+        for comment in submission.comments:
+            findFeet(comment.body)
+
+main()
